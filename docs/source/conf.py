6e4a2c641e2b2f834a7bbf856c3aa030d37dfff5
Ioannis Tziakos <ioannist@enthought.com>
2015-08-26 14:18:41 +0100

    create and use a custom sectiondoc style

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,8 +17,8 @@ import os
 # If your extensions are in another directory, add it here. If the directory
 # is relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
-sys.path.append(os.path.abspath('_extensions'))
 sys.path.append(os.path.abspath('../../'))
+sys.path.append(os.path.abspath('.'))
 
 
 def mock_modules():
@@ -118,10 +118,11 @@ mock_modules()
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = [ 'refactordoc',
-               'sphinx.ext.viewcode',
-               'sphinx.ext.autosummary',
-               'traits.util.trait_documenter']
+extensions = [
+    'docstyle',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary',
+    'trait_documenter']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
